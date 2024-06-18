5ec14514b84e92c6c339b15b9a12bfd940dc1958
Juan José Angarita Yela <129020936+RamonaFlores@users.noreply.github.com>
2024-06-18 16:08:54 -0500

    added actual Links to the hrefs of the website!!!

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Vid from './components/Vid';
+
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 import { Container } from 'react-bootstrap';
 import HomeScreen from './screens/HomeScreen';
--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -2,20 +2,32 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import {LinkContainer} from 'react-router-bootstrap';
 function Header() {
     return (
       <header>
         <br />
         <Navbar bg="light" data-bs-theme="light" collapseOnSelect>
           <Container>
-            <Navbar.Brand href="#home">Offside</Navbar.Brand>
+            <LinkContainer to='/'>
+              <Navbar.Brand>Offside</Navbar.Brand>
+            </LinkContainer >
             <Nav className="me-auto">
-              <Nav.Link href="#retros">Retro Kits</Nav.Link>
-              <Nav.Link href="#FAQ">FAQ</Nav.Link>
+              <LinkContainer to="/retros">
+                <Nav.Link>Retro Kits</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/FAQ">
+                <Nav.Link>FAQ</Nav.Link>
+              </LinkContainer>
             </Nav>
             <Nav className="ml-auto">
-              <Nav.Link href="#Login"><i className='fas fa-user'></i> Login</Nav.Link>
-              <Nav.Link href="#Cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
+              <LinkContainer to="/Login">
+                    <Nav.Link><i className='fas fa-user'></i> Login</Nav.Link>
+              </LinkContainer>
+              
+              <LinkContainer to="/Cart">
+                    <Nav.Link><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
+              </LinkContainer>
             </Nav>
           </Container>
         </Navbar>
