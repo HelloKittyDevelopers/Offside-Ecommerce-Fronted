b312505cc70bd4daa7213c469204eed2e96637be
Juan José Angarita Yela <129020936+RamonaFlores@users.noreply.github.com>
2024-06-19 21:54:47 -0500

    working Get Info!!!
    now the Front fetches the products from dynamic data and not from the static objects. thats huge..

---
+++ backend/base/products.py
@@ -0,0 +1,19 @@
+products = [
+    {
+        "_id": "1",
+        "name": "Retro Jersey 1",
+        "image": "/images/jersey1.png",
+        "description": "High quality retro football jersey.",
+        "brand": "Retro Brand",
+        "category": "Jerseys",
+        "price": 89.99,
+        "rating": 4.5,
+        "numReviews": 12,
+        "sizes": [
+            {"size": "S", "countInStock": 0},
+            {"size": "M", "countInStock": 0},
+            {"size": "L", "countInStock": 0},
+            {"size": "XL", "countInStock": 0},
+        ],
+    },
+]
--- front/src/index.js
+++ front/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; // Importa ReactDOM de esta manera
+import ReactDOM from 'react-dom/client'; // Correct import for React 18
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
