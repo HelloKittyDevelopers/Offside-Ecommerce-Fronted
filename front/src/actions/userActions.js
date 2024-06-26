9e12c316da8e9dc53636ae2b072177dcff06b14d
geoffrey0pv <geoffreypv00@gmail.com>
2024-06-26 01:52:08 -0500

    feat: first steps to make a user login & user register view

---
+++ front/src/actions/userActions.js
@@ -0,0 +1,40 @@
+import { USER_LOGIN_REQUEST,
+    USER_LOGIN_SUCCESS,
+    USER_LOGIN_FAIL,
+    USER_LOGIN_LOGOUT } from '../constants/usersConstants'
+
+import axios from 'axios';
+
+
+    export const login = (username, password) => async(dispatch) => {
+        try{
+            dispatch({
+                type: USER_LOGIN_REQUEST
+
+            })
+
+            const config = {
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            }
+
+            const {data} = await axios.post('/users/login'
+                {'username':username,'password': password
+                }
+            )
+
+            dispatch({
+                type:USER_LOGIN_SUCCESS,
+                payload: data
+            })
+
+            localStorage.setItem('userInfo', JSON.stringify(data))
+
+        } catch (error) {
+            dispatch({
+              type: USER_LOGIN_FAIL,
+              payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+            });
+          }
+    }
--- front/src/store.js
+++ front/src/store.js
@@ -1,13 +1,38 @@
 import { configureStore } from '@reduxjs/toolkit';
 import {productListReducer} from './reducers/productReducers';
+import { userLoginReducer } from './reducers/userReducers'
+import {productListReducer} from './reducers/productReducers'; // Asegúrate de que este sea el nombre correcto del reducer
 import productDetailsReducer from './reducers/productDetailReducer';
+import { cartReducer } from './reducers/cartReducer';
+
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+    ? JSON.parse(localStorage.getItem('cartItems'))
+    : [];
+
+
+    const userInfoFromStorage = localStorage.getItem('userInfo') 
+    ? JSON.parse(localStorage.getItem('userInfo')) 
+    : null
+
+    
+const preloadedState = {
+    cart: {
+        cartItems: cartItemsFromStorage,
+    },
+    userLogin: {
+      userInfo: userInfoFromStorage
+    }
+};
 
 const store = configureStore({
-  reducer: {
-    productList: productListReducer,
-    productDetails: productDetailsReducer,
-  },
-  devTools: process.env.NODE_ENV !== 'production',
+    reducer: {
+        productList: productListReducer,
+        productDetails: productDetailsReducer,
+        cart: cartReducer,
+        userLogin: userLoginReducer,
+    },
+    preloadedState,
+    devTools: process.env.NODE_ENV !== 'production',
 });
 
 export default store;
