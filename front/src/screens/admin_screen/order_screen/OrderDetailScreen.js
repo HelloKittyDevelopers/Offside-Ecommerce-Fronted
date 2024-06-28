c1e98ab7f8df238f44a033c65d5ccdfe8945c616
juanex2001 <juanseg201@gmail.com>
2024-06-27 22:20:12 -0500

    feat: Order Screen Done

--- front/src/screens/admin_screen/order_screen/OrderDetailScreen.js
+++ front/src/screens/admin_screen/order_screen/OrderDetailScreen.js
@@ -4,10 +4,9 @@ import { Panel } from 'primereact/panel';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import OrderService from '../../../service/OrderService';
-import UserService from '../../../service/UserService';
 import OrderItemService from '../../../service/OrderItemService';
-import ProductService from '../../../service/ProductService';
 import OrderStateService from '../../../service/OrderStatesService';
+import ProductService from '../../../service/ProductService';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
@@ -16,29 +15,26 @@ import '../AdminScreen.css';
 const OrderDetailScreen = () => {
   const { id_order } = useParams();
   const [order, setOrder] = useState(null);
-  //const [user, setUser] = useState(null);
   const [orderItems, setOrderItems] = useState([]);
   const [orderState, setOrderState] = useState([]);
-  
+  const [products, setProducts] = useState([]);
+
   useEffect(() => {
     const fetchOrderDetails = async () => {
       try {
         const orderData = await OrderService.getOrderById(id_order);
-        console.log(orderData)
         setOrder(orderData);
 
-        //const userData = await UserService.getById(orderData.user);
-        //console.log(userData)
-        //setUser(userData);
+        const orderStateData = await OrderStateService.getOrderStateById(orderData.order_state);
+        setOrderState(orderStateData);
 
-        const orderState = await OrderStateService.getOrderStateById(orderData.order_state)
-        setOrderState(orderState)
-
-        const orderItemsData = await OrderItemService.getOrderItemById(id_order);
-        console.log(orderItemsData)
+        const orderItemsData = await OrderItemService.getOrderItemsByOrderId(id_order);
         setOrderItems(orderItemsData);
 
-        const productsData = await ProductService.getProductById()
+        // Fetch product details for each order item
+        const productPromises = orderItemsData.map(item => ProductService.getProductById(item.product));
+        const productsData = await Promise.all(productPromises);
+        setProducts(productsData);
       } catch (error) {
         console.error('Error fetching order details:', error);
       }
@@ -51,19 +47,24 @@ const OrderDetailScreen = () => {
     return <div>Loading...</div>;
   }
 
+  const orderItemsWithProductDetails = orderItems.map(item => {
+    const product = products.find(prod => prod.id_product === item.product);
+    return { ...item, product };
+  });
+
   return (
-    <Panel header={`Order Details`} style={{ width: '80%', margin: '20px auto', height: '100%' }}>
+    <Panel header="Order Details" style={{ width: '80%', margin: '20px auto', height: '100%' }}>
       <div>
         <h3>Order ID: {order.id_order}</h3>
         <p>Date: {order.date_order}</p>
         <p>Total Payment: {order.total_payment + '$'}</p>
         <p>Shipping Price: {order.shipping_price + '$'}</p>
-        <p>Taxes: {order.taxes+'%'}</p>
+        <p>Taxes: {order.taxes + '%'}</p>
         <p>Order Status: {orderState.order_state}</p>
       </div>
       <div>
         <h3>Order Items</h3>
-        <DataTable value={orderItems} header="Order Items">
+        <DataTable value={orderItemsWithProductDetails} header="Order Items">
           <Column field="product.product_name" header="Product Name" sortable />
           <Column field="order_quantity" header="Quantity" sortable />
           <Column field="product.price" header="Price" sortable />
